2e2094dd925d81147b4e44e38d8bb01b8f3d37b9
ornelasf1 <ornelasf1@gmail.com>
2019-09-15 20:46:26 -0500

    Center input components vertically with top and position properties

--- src/components/ConversionAlgorithm.js
+++ src/components/ConversionAlgorithm.js
@@ -45,7 +45,7 @@ class ConversionAlgorithm extends React.Component {
     handleNotationButtons = (_, selected, toNotation) => {
         this.props.updateToNotation(toNotation);
         this.setState({toNotation, instructions: getInstructionSet(selected, toNotation)});
-        const notationComps = document.getElementById('conversion-comp');
+        const notationComps = document.getElementsByClassName('App')[0];
         if (notationComps.style.animationName === '') {
             notationComps.style.animationName = 'elevateBoxes';
         }
